8f95660f7012673e5a049e92b392950376f7f532
baconYao <bacon735392@gmail.com>
2016-03-05 18:01:53 +0800

    Finish

---
+++ HW1/appServer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // for bzero
+#include <unistd.h>  // for close()
+#include <sys/types.h>  
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PortNumber 3000
+
+int main(int argc, char *argv[]){
+  struct sockaddr_in address;
+  int sock,byte_recv,address_length = sizeof(address);
+  int datagramCount = 0;
+  // 配置記憶體
+  // char *buffer = malloc(bufferSize * sizeof(char));
+  char buffer[50];
+  char overSignal[10] = "Complete!";
+    
+
+  // 建立socket
+  sock = socket(PF_INET,SOCK_DGRAM,0);
+  if(sock < 0) {
+    printf("Error creating socket\n");
+    exit(1);
+  ;}
+
+  bzero(&address, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(PortNumber);
+  address.sin_addr.s_addr = INADDR_ANY;
+
+  if(bind(sock, (struct sockaddr *)&address, sizeof(address)) == -1){
+    printf("error binding\n");
+    close(sock);
+  }
+
+  while(1){
+    byte_recv = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&address, &address_length);
+    // 接收失敗
+    if(byte_recv < 0) {
+        printf("Error recving packet\n");
+    }
+    
+    // Client傳送完畢
+    if(strspn(buffer,overSignal)){
+      // 釋放記憶體
+      // free(buffer);
+      printf("Client send completed!\n");
+      datagramCount = 0;
+    }
+    else{
+      printf("Datagram %d : %s\n",datagramCount,buffer);
+      datagramCount++;
+    }
+  }
+
+  close(sock);
+  return 0;
+}
---
+++ HW1/readme.md
@@ -0,0 +1,16 @@
+#ReadMe
+---
+###	`Author` : Pei-Yao Chang
+###	`Grade`	:	NCHU of fourth grade
+###	`Student ID` : 4101056036
+###	`Finish Date` : 2016/2/28
+###	`Language` : C language		
+		
+##	Related Work :
+*	[htonl() 、 htons() 、 inet_ntoa() 、 inet_addr() 用法](http://blog.csdn.net/bzhxuexi/article/details/20460655)
+* 	[bind()](http://c.biancheng.net/cpp/html/353.html)
+*  [sendto()](http://c.biancheng.net/cpp/html/372.html)
+*  [gettimeofday()](http://www.cnblogs.com/wainiwann/p/4342212.html)
+*  [gettimeofday()-2](http://blog.csdn.net/tigerjibo/article/details/7039434)
+*  [網路單位詳解](http://www.spring.url.tw/internet/internet_06.html)
+*  [AF_INET 與 PF_INET的差異](http://spyker729.blogspot.tw/2010/07/afinetpfinet.html)
