68b022a641c85ec88c83e9a0927e50c5dcc2d32b
baconYao <bacon735392@gmail.com>
2016-05-25 12:12:59 +0800

    test

---
+++ HW4/hw4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/time.h>       //for gettimeofday()
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>          //for gethostbyname()
+
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in to;
+
+
+
+    return 0;
+}
