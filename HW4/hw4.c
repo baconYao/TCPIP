b6a8d6d52358c329c1b16ec314e44dda4fa50ba8
baconYao <bacon735392@gmail.com>
2016-05-28 21:21:52 +0800

    a huge bug

--- HW4/hw4.c
+++ HW4/hw4.c
@@ -16,6 +16,18 @@ int main(int argc, char *argv[])
 {
     int sock;
     struct sockaddr_in to;
+    int to_len = sizeof(to);
+    char buffer[65536];
+    struct timeval start_time,next_time,back_time;
+    int rtt=0;
+    int s_time = 0, n_time = 0;
+    struct icmp *icmp;
+    struct ip *ip = (struct ip *)buffer;
+
+
+    sock(PF_INET,SOCK_RAW,1);
+
+
 
 
 
