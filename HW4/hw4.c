2748ce3c842c75e7b98cfc1b16e2602ee2f6261f
baconYao <bacon735392@gmail.com>
2016-05-31 17:05:09 +0800

    new

--- HW4/hw4.c
+++ HW4/hw4.c
@@ -11,13 +11,116 @@
 #include <unistd.h>
 #include <netdb.h>          //for gethostbyname()
 
+int getIp(void);        //取得目的端的IP address
+unsigned short cksum(unsigned short *, int);    //計算CHECKSUM
+int rtt_c(int,int,int,int);     //計算ROUND TRIP TIME
+
+char *hostip;
+struct sockaddr_in to;
+int to_len = sizeof(to);
 
 int main(int argc, char *argv[])
 {
     int sock;
-    struct sockaddr_in to;
+    int seqnum = 0;           //SEQ Number從0開始
+    char buffer[65536];
+    struct timeval start_time,next_time,back_time;
+    int rtt=0;
+    int s_time = 0, n_time = 0;
+    struct icmp *icmp;
+    struct ip *ip = (struct ip *)buffer;
+
+    //Use ICMP packet
+    sock(PF_INET,SOCK_RAW,1);
+    if(sock == -1)
+    {
+        perror("Socket can't creat!\n");
+        exit(1);
+    }
+
+    // 取得Address
+    if(getIp() == 0)
+    {
+        exit(1);
+    }
+
+    printf("Pining %s\n\n", argv[1]);
+    //將Buffer初始化
+    bzero(buffer,buffersize + 16);
+    //轉為ICMP格式
+    icmp = (struct icmp *)buffer;
+    
 
 
+
+    
 
     return 0;
+}
+
+
+int getIp()
+{
+    char *hostname;
+    struct in_addr taraddr;
+    struct hostent *hp;
+    int j;
+
+
+    hp = gethostbyname(argv[1]);           //查詢hostname的地址
+    if(hp) {
+       hostname = hp->h_name;
+       to.sin_family = hp->h_addrtype;
+       bcopy(hp->h_addr,(caddr_t)&to.sin_addr,hp->h_length);
+       for(j = 0; hp->h_addr_list[j]; j++) {
+          memcpy(&taraddr.s_addr, hp->h_addr_list[j], hp->h_length);
+          hostip = inet_ntoa(taraddr);   
+       }
+       return 1;
+    }
+    else {                                 //DESTINATION輸入錯誤
+       printf("ping: unknown host %s\n", argv[1]);
+       return 0;
+    }
+
+
+}
+
+
+//計算ICMP封包的CHECKSUM
+unsigned short cksum(unsigned short *section, int icmp_byte)
+{
+   int sum = 0;
+   unsigned short *sec;
+   unsigned short temp;
+   unsigned short answer = 0;
+
+   sec = section;            //每16bits為一個Section來計算
+ 
+   while(icmp_byte > 1) {
+      sum += *sec++;
+      icmp_byte -= 2;
+   }
+   if(icmp_byte == 1) {      //最後一個Section只有一個byte
+      *(unsigned char *)&temp = *(unsigned char *)sec;
+      sum += temp;
+   }
+
+   sum = (sum >> 16) + (sum & 0xffff);      //將進位加回Partial SUM
+   sum += (sum >> 16);                      //可能又產生進位
+   answer = ~sum;                           //取Complement
+
+   return answer;
+}
+
+//計算ROUND TRIP TIME, 以毫秒顯示
+int rtt_c(int arr_time, int rtime, int ttime, int dep_time)
+{
+   /* 
+   arr_time:time the packet returned
+   rtime:transmit timestamp
+   ttime:receive timestamp
+   dep_time:original timestamp
+   */
+   return (int) ((arr_time - rtime) + (ttime - dep_time));
 }
--- hw4.c
+++ hw4.c
@@ -23,32 +23,32 @@ int main(int argc, char *argv[])
 {
    int sock;
  
-   struct sockaddr_in address;
-   int address_length = sizeof(address);
+   // struct sockaddr_in address;
+   // int address_length = sizeof(address);
 
 
    struct in_addr taraddr;
    struct sockaddr_in to;
    char buffer[65535];
    struct icmp *icmp;
-   struct ip *ip = (struct ip *)buffer;
+   struct ip *ip;
    struct timeval start_time;
    int s_time=0,r_time=0;
    struct timeval curr_time;
    struct timeval timeout;
-   int recv_tim,tran_tim;
    int seqnum = 0;           //SEQ Number從0開始
-   int buffersize = 32;      //預設Buffer Size為32 bytes
+   int buffersize = 32;      //預設Buffer Size為32 bytes (20:IP Header, 8:ICMP Header , 4: Original timestamp)
    struct hostent *hp;
    int rtt=0;
    char *hostip;
    int i, j;
+   char *pbuffer;
 
    if(argc < 2) {            //輸入格式錯誤
-      printf("Format: ./pingHW IP_Address\n");
+      printf("Format:sudo ./pingHW IP_Address\n");
       exit(0);
    }
-   argc--;
+   // argc--;
    i = 1;
 
    //各個參數的擷取
@@ -70,12 +70,17 @@ int main(int argc, char *argv[])
       i++;
    }*/
  
-   sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+   sock = socket(AF_INET, SOCK_RAW, 1);
+   if(sock == -1)
+   {
+      perror("Socket can't creat!\n");
+      exit(1);
+   }
    hp = gethostbyname(argv[i]);           //查詢hostname的地址
    if(hp) {
       hostname = hp->h_name;
       to.sin_family = hp->h_addrtype;
-      to.sin_port = htons(5555);
+      // to.sin_port = htons(5555);
       bcopy(hp->h_addr,(caddr_t)&to.sin_addr,hp->h_length);
       for(j = 0; hp->h_addr_list[j]; j++) {
          memcpy(&taraddr.s_addr, hp->h_addr_list[j], hp->h_length);
@@ -88,18 +93,18 @@ int main(int argc, char *argv[])
    }
 
    // test
-   bzero(&address, sizeof(address));
-   address.sin_family = AF_INET;
-   // 如果 = 0，則代表自動分配port number
-   address.sin_port = htons(5555);
-   // 自動填入本機位址
-   address.sin_addr.s_addr = INADDR_ANY;
+   // bzero(&address, sizeof(address));
+   // address.sin_family = AF_INET;
+   // // 如果 = 0，則代表自動分配port number
+   // address.sin_port = htons(5555);
+   // // 自動填入本機位址
+   // address.sin_addr.s_addr = INADDR_ANY;
 
-   // bind():給訂socket一個名稱
-   if(bind(sock, (struct sockaddr *)&address, sizeof(address)) == -1){
-     printf("error binding\n");
-     close(sock);
-   }
+   // // bind():給訂socket一個名稱
+   // if(bind(sock, (struct sockaddr *)&address, sizeof(address)) == -1){
+   //   printf("error binding\n");
+   //   close(sock);
+   // }
    // test
 
 
@@ -110,18 +115,20 @@ int main(int argc, char *argv[])
    printf("Pining %s\n\n", argv[i]);
 
    //送出和接收ICMP封包
-   for(i = 0; i < maxcount; i++) {
+   // for(i = 0; i < maxcount; i++) {
       bzero(buffer,buffersize + 16);         //將Buffer全填零
-      icmp = (struct icmp *)buffer;
+      // pbuffer = buffer+20;
+      icmp = (struct icmp *)buffer;          //轉為ICMP格式
     
       //ECHO_REQUEST MESSAGEShtol()
-      icmp->icmp_type = 13;
+      // 填入ICMP的預設內容
+      icmp->icmp_type = 13;                  //ICMP Request
       icmp->icmp_code = 0;
       icmp->icmp_id = getpid();              //ID設為Process ID
       icmp->icmp_seq = seqnum++;             //SEQ Number從零開始，下一個加一
       //gettimeofday(&start_time,NULL);
       gettimeofday(&start_time, NULL);      //建立時間
-      s_time=(start_time.tv_sec%(24*60*60))*1000+start_time.tv_usec/1000;         //設定並且轉換時間格式
+      s_time=(start_time.tv_sec % (24*60*60)) * 1000 + (start_time.tv_usec/1000);         //設定並且轉換時間格式，單位:毫秒
       icmp->icmp_otime=htonl(s_time);   //將時間放入icmp_otime內
       icmp->icmp_rtime=0;		//icmp_rtime設為0
       icmp->icmp_ttime=0;		//icmp_ttime設為0
@@ -140,11 +147,19 @@ int main(int argc, char *argv[])
       printf("Previous RTT:%d\n",rtt);
       printf("----------------------\n");
       
-      // recvfrom(sock, buffer,sizeof(buffer), 0, (struct sockaddr*)&address, &address_length);
-
-      recvfrom(sock, buffer,sizeof(buffer), 0, NULL, NULL);       //接收封包
+      int recv;
+      
+      recv = recvfrom(sock, buffer,sizeof(buffer), 0, NULL, NULL);       //接收封包
+      if(recv < 0)
+      {
+         printf("Receive Error!\n");
+      }
+      // printf("buffer : %ld\n",sizeof(buffer));
+      ip = (struct ip *)buffer;
       icmp = (struct icmp*)(buffer + ip->ip_hl*4);
-
+      printf("%d\n", icmp->icmp_rtime);
+      printf("%d\n", icmp->icmp_ttime);
+      printf("%d\n", s_time);
       //設定Back Time
       gettimeofday(&curr_time, NULL);
       //轉換時間格式
@@ -162,7 +177,7 @@ int main(int argc, char *argv[])
       printf("======================\n\n");     
 
       sleep(1);        //一秒送一次
-   }
+   // }
    
 printf("End of Ping to %s\n",hostip);
    return 0;
@@ -195,6 +210,12 @@ unsigned short cksum(unsigned short *section, int icmp_byte)
 //計算ROUND TRIP TIME, 以毫秒顯示
 int rtt_c(int arr_time, int rtime, int ttime, int dep_time)
 {
+   /* 
+   arr_time:time the packet returned
+   rtime:transmit timestamp
+   ttime:receive timestamp
+   dep_time:original timestamp
+   */
    return (int) ((arr_time - rtime) + (ttime - dep_time));
 }
 
